c537d6bac8a7b7b232381ce1eeea211c4053d4a1
OSUpdate <force185@naver.com>
2020-09-22 21:38:28 +0900

    2020-09-22

--- Backjoon/1932.py
+++ Backjoon/1932.py
@@ -1,14 +1,19 @@
 from sys import stdin
+from copy import deepcopy
 
 input = stdin.readline
 
 n = int(input())
-pre = 0
+tmp = []
 dp = []
 for i in range(n):
-    tmp = list(map(int,input().split()))
-    if len(tmp) == 1:
-        pre = tmp[0]
-    else:
-        for v in tmp:
-            pre = v
+    tmp.append(list(map(int,input().split())))
+    dp.append([0 for _ in range(n)])
+dp[0][0] = tmp[0][0]
+for i in range(1,len(tmp)):
+    for j in range(len(tmp[i])):
+        if j == 0:
+            dp[i][j] = dp[i-1][j]+tmp[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j]+tmp[i][j],dp[i-1][j-1] + tmp[i][j])
+print(max(dp[n-1]))
