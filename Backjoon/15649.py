71e0e486f8416f19b2577a36b30a8d03b2177164
OSUpdate <34418035+OSUpdate@users.noreply.github.com>
2020-09-29 16:29:11 +0900

    2020-09-29

---
+++ Backjoon/15649.py
@@ -0,0 +1,23 @@
+from sys import stdin
+from collections import deque
+
+input = stdin.readline
+
+n,m = map(int,input().split())
+
+arr = [i for i in range(1,n+1)]
+visit = [False] * n
+ans = []
+def dfs(cnt):
+    if cnt == m:
+        print(' '.join(map(str,ans)), end='\n')
+        return
+    for i,v in enumerate(arr):
+        if not visit[i]:
+            visit[i] = True
+            ans.append(v)
+            dfs(cnt+1)
+            visit[i] = False
+            ans.pop()
+dfs(0)
+            
