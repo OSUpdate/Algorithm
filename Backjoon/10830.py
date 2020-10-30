aeaad7cbfe734b7e000bf2b87ca828b21e4a3801
OSUpdate <34418035+OSUpdate@users.noreply.github.com>
2020-10-30 23:13:40 +0900

    2020-10-30

--- Backjoon/10830.py
+++ Backjoon/10830.py
@@ -38,3 +38,38 @@ for i in range(n):
     for j in range(n):
         print(ans[i][j], end=" ")
     print()
+'''
+from sys import stdin
+
+input = stdin.readline
+
+n,b = map(int,input().split())
+arr = []
+def pow(a,b):
+    global n 
+    ans = [[0 for _ in range(n)] for _ in range(n)]
+    print(a,b)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                ans[i][j] += a[i][k] * b[k][j]
+            ans[i][j] %= 1000
+    print(ans)
+    return ans
+def matrix(b):
+    global arr,n
+    res = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+    while b>0:
+
+        if b % 2 == 1:
+            b -=1
+            res = pow(res,arr)
+        else:
+            b //=2
+            arr = pow(arr,arr)
+        
+    return res
+for _ in range(n):
+    arr.append(list(map(int,input().split()))) 
+print(matrix(b))
+'''
